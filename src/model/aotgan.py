335a1ac689e43af618b74a37c48ca582f6de8768
qjl <qjl.qqcom>
2022-10-06 11:38:14 +0800

    v2.0

--- src/model/aotgan.py
+++ src/model/aotgan.py
@@ -54,6 +54,31 @@ class UpConv(nn.Module):
     def forward(self, x):
         return self.conv(F.interpolate(x, scale_factor=2, mode='bilinear', align_corners=True))
 
+class se_block(nn.Module):
+    def __init__(self, channel, ratio=1):
+        super(se_block, self).__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(channel, channel // ratio, bias=False),
+
+            # nn.BatchNorm1d(channel//ratio, eps=1e-05, momentum=0.1, affine=True),
+
+        # nn.BatchNorm1d(channel//ratio, eps=1e-5,momentum=0.1, affine=True, track_running_stats=True),
+
+            nn.ReLU(inplace=True),
+            nn.Linear(channel // ratio, channel, bias=False),
+            nn.Sigmoid()
+        )
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        print(x.size())
+        y = self.avg_pool(x).view(b, c)
+        print(y.size())
+        y = self.fc(y).view(b, c, 1, 1)
+        print(y.size())
+        return x * y
 
 class AOTBlock(nn.Module):
     def __init__(self, dim, rates):
@@ -76,10 +101,19 @@ class AOTBlock(nn.Module):
 
     def forward(self, x):
         out = [self.__getattr__(f'block{str(i).zfill(2)}')(x) for i in range(len(self.rates))]
+        # out = torch.cat(out, 1)
+        # out = self.fuse(out)
+        # mask = my_layer_norm(self.gate(x))
+        # mask = torch.sigmoid(mask)
+        # return x * (1 - mask) + out * mask
         out = torch.cat(out, 1)
         out = self.fuse(out)
         mask = my_layer_norm(self.gate(x))
-        mask = torch.sigmoid(mask)
+        b, c, _, _ = mask.shape
+        se = se_block(c)
+        out1 = se(mask)
+        mask = torch.sigmoid(out1)
+
         return x * (1 - mask) + out * mask
 
 
